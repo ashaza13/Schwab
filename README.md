c27e8fbfc473bbf2952f6f3b299b3ea3afdfaefe
ashaza13 <94416376+ashaza13@users.noreply.github.com>
2023-06-29 16:55:49 -0400

    Commit files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Schwab Index Fund Price Prediction
---
+++ split.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("SWPPX.csv")
+
+df['split'] = np.random.randn(df.shape[0], 1)
+
+msk = np.random.rand(len(df)) <= 0.7
+
+train = df[msk]
+test=df[~msk]
+
+train.to_csv('train.csv', index=False)
+test.to_csv('test.csv', index=False)
+
+print(train)
